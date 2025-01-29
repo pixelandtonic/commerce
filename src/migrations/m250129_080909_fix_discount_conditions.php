02336318c8cd83d55a9bb0e42561517f8a33deb1
Luke Holder <lukemh@gmail.com>
2025-01-29 16:35:55 +0800

    Use discount storeId

--- src/migrations/m250129_080909_fix_discount_conditions.php
+++ src/migrations/m250129_080909_fix_discount_conditions.php
@@ -2,7 +2,6 @@
 
 namespace craft\commerce\migrations;
 
-use Craft;
 use craft\commerce\db\Table;
 use craft\db\Migration;
 use craft\db\Query;
@@ -18,27 +17,22 @@ class m250129_080909_fix_discount_conditions extends Migration
      */
     public function safeUp(): bool
     {
-        $primaryStoreId = (new Query())
-            ->select('id')
-            ->from(Table::STORES)
-            ->where(['primary' => true])
-            ->scalar();
-
-        // get orderCondition from craft_commerce_discounts table
         $discounts = (new Query())
-            ->select(['id', 'orderCondition'])
+            ->select(['id', 'orderCondition', 'storeId'])
             ->from(Table::DISCOUNTS)
-            ->pairs();
+            ->all();
 
-        foreach ($discounts as $discountId => $orderCondition) {
-            $orderConditionData = Json::decodeIfJson($orderCondition);
+        foreach ($discounts as $discount) {
+            $discountId = $discount['id'];
+            $storeId = $discount['storeId'];
+            $orderConditionData = Json::decodeIfJson($discount['orderCondition']);
 
             if (!is_array($orderConditionData)) {
                 continue;
             }
 
             if (!isset($orderConditionData['storeId'])) {
-                $orderConditionData['storeId'] = $primaryStoreId;
+                $orderConditionData['storeId'] = $storeId;
                 $orderConditionJson = Json::encode($orderConditionData);
                 $this->update(Table::DISCOUNTS,
                     ['orderCondition' => $orderConditionJson],
